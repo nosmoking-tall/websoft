061af95a5208e0a3db25d1f11b6f23caec906d73
Huifeng Wu <1647505311@qq.com>
2020-03-21 11:02:42 +0100

    update css

--- work/report/js/duck.js
+++ work/report/js/duck.js
@@ -1,12 +1,58 @@
-/**
- * a Comment
- */
-"use strict";
 (function () {
-    var element = document.getElementById("duck");
-    console.log("Duck ready");
-    element.addEventListener("click",function () {
-        console.log("duck clicked");
-    });
+    'use strict';
 
+    var area = document.body,
+        areaHeight = window.innerHeight,
+        areaWidth = window.innerWidth,
+        points = document.getElementById('points'),
+        duck = document.createElement('img'),
+        score = 0,
+        timer = 1000;
+
+
+    /**
+     * Keep track on score.
+     */
+
+
+    /**
+     * Set the attributes for the duck
+     **/
+    duck.src='img/duck.png';
+    duck.style.position ='absolute';
+    duck.style.left = '0px';
+    duck.style.top = '0px';
+    duck.style.zIndex = 10000;
+
+
+
+    /**
+     * A function for displaying the duck in random positions
+     **/
+    function newDuck() {
+        var newX = Math.floor(Math.random() * (areaWidth-duck.width)),
+            newY = Math.floor(Math.random() * (areaHeight-duck.height));
+
+        duck.style.left = newX+'px';
+        duck.style.top = newY+'px';
+        area.appendChild(duck);
+    }
+
+
+
+    /**
+     * The function that triggers the game, uses an time interval in milliseconds
+     **/
+    function startGame() {
+        window.setInterval(newDuck, timer);
+    }
+
+
+
+    /**
+     * Start the game
+     **/
+    startGame();
+
+    console.log('Game is ready!');
 })();
